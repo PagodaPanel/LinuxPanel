ad89a07de5acdc57466c14db6b4b6661154c7e21
PagodaPanel <PagodaPanel@users.noreply.github.com>
2022-06-23 19:20:04 +0800

    Fix layout and remove force bind

--- class/public.py
+++ class/public.py
@@ -3018,7 +3018,7 @@ def run_thread(fun,args = (),daemon=False):
     return True
 
 def check_domain_cloud(domain):
-    run_thread(cloud_check_domain,(domain,))
+    return
 
 def cloud_check_domain(domain):
     '''
@@ -3064,8 +3064,7 @@ def get_user_info():
     return userInfo
 
 def is_bind():
-    # if not os.path.exists('{}/data/bind.pl'.format(get_panel_path())): return True
-    return not not get_user_info()
+    return True
 
 
 def send_file(data,fname='',mimetype = ''):
