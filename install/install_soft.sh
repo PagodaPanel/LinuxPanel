68cafa0f45986fb94cf85913de0ff644231fa1c5
PagodaPanel <pagodapanel@users.noreply.github.com>
2022-08-02 13:58:22 +0800

    Fix

--- install/install_soft.sh
+++ install/install_soft.sh
@@ -23,7 +23,6 @@ if [ "$actionType" == 'install' ];then
 fi
 
 sed -i "s/http:\/\/download.bt.cn\/install\/public.sh/http:\/\/cdn.jsdelivr.net\/gh\/PagodaPanel\/Pagoda\/install\/public.sh/" lib.sh
-sed -i "/wget -O Tpublic.sh/d" $name.sh
 
 bash $name.sh $actionType $version
 echo '|-Successify --- 命令已执行! ---'
