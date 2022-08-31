88a77355c3d4bdbafe44b4faa984f76ec52ad94c
PagodaPanel <pagodapanel@users.noreply.github.com>
2022-09-01 01:36:50 +0800

    Skipped check.sh

--- install/install_soft.sh
+++ install/install_soft.sh
@@ -22,6 +22,7 @@ sed -i "s/http:\/\/download.bt.cn\/install\/public.sh/http:\/\/cdn.jsdelivr.net\
 wget -O $name.sh $serverUrl/$mtype/$name.sh
 sed -i 's|\\cp -rpa|echo skipped #\\cp -rpa|' $name.sh
 #sed -i '/Tpublic.sh/d' $name.sh
+sed -i 's|wget -O ${checkFile}|echo skipped #wget -O ${checkFile}|' $name.sh
 
 if [ "$actionType" == 'install' ];then
 	bash lib.sh
